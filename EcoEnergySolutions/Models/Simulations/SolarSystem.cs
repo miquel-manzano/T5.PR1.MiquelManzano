8a5194595a8d5701472cd44074648f12d7269e7b
miquel-manzano <miquel.manzano.7e6@itb.cat>
2025-03-25 17:11:31 +0100

    Solution import finished Closes #1

---
+++ EcoEnergySolutions/Models/Simulations/SolarSystem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimulationsLibrary
+{
+    public class SolarSystem: EnergySystem, IEnergyCalculation
+    {
+        private double SunHours;
+
+        public SolarSystem() : base("Solar") { }
+
+        public override void SetParameters()
+        {
+            Console.Write("Introdueix les hores de sol disponibles (> 1): ");
+            SunHours = UserInteractionHelper.UserDoubleInput(1);//user library
+        }
+
+        public override double CalculateEnergy()
+        {
+            return SunHours * 1.5;
+        }
+    }
+}
